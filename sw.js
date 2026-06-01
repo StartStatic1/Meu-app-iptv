d24aafb68fea0e58c508b372e0d0607e2537c029
StartStatic1 <startstatic@outlook.com>
2026-06-01 10:03:57 -0300

    Implement service worker caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,6 +1,37 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11081847
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+const CACHE_NAME = 'streamflix-cache-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/style.css',
+                '/js/app.js',
+                '/js/config.js'
+            ]);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
