ecb0b258abba646112d2f4cbb816198846a48c42
StartStatic1 <startstatic@outlook.com>
2026-06-06 10:41:56 -0300

    Add files via upload

---
+++ js/iptv.js
@@ -0,0 +1,63 @@
+// api/iptv.js - MOTOR 7.0 (SISTEMA DE FALLBACK)
+export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Content-Type', 'application/json');
+
+    const { action, category_id, stream_id, series_id, extension } = req.query;
+
+    const servidores = [
+        { url: "http://kavru.com:80", user: "558396043519", pass: "64537505" },
+        { url: "http://rnplay07.vip:80", user: "941394441", pass: "903228872" },
+        { url: "http://bnewsc.top:80", user: "reginaldobr", pass: "432334xc" }
+    ];
+
+    if (action === "get_movie_url" && stream_id) {
+        const ext = extension || "mp4";
+        const svr = servidores[0];
+        return res.status(200).json({ url: `${svr.url}/movie/${svr.user}/${svr.pass}/${stream_id}.${ext}` });
+    }
+
+    if (action === "get_live_url" && stream_id) {
+        const svr = servidores[0];
+        return res.status(200).json({ url: `${svr.url}/${svr.user}/${svr.pass}/${stream_id}.ts` });
+    }
+
+    if (action === "get_series_url" && stream_id) {
+        const ext = extension || "mp4";
+        const svr = servidores[0];
+        return res.status(200).json({ url: `${svr.url}/series/${svr.user}/${svr.pass}/${stream_id}.${ext}` });
+    }
+
+    let erroFinal = "Todos os servidores falharam.";
+
+    for (let i = 0; i < servidores.length; i++) {
+        const svr = servidores[i];
+        let targetUrl = `${svr.url}/player_api.php?username=${svr.user}&password=${svr.pass}`;
+
+        if (action) targetUrl += `&action=${action}`;
+        if (category_id) targetUrl += `&category_id=${category_id}`;
+        if (series_id) targetUrl += `&series_id=${series_id}`;
+
+        try {
+            const controller = new AbortController();
+            const timeoutId = setTimeout(() => controller.abort(), 6000);
+
+            const response = await fetch(targetUrl, { signal: controller.signal });
+            clearTimeout(timeoutId);
+
+            if (response.ok) {
+                const data = await response.json();
+                return res.status(200).json(data);
+            } else {
+                erroFinal = `Servidor ${i + 1} retornou Erro ${response.status}`;
+                console.log(erroFinal);
+            }
+        } catch (error) {
+            erroFinal = `Servidor ${i + 1} Offline ou Demorou muito.`;
+            console.log(erroFinal);
+        }
+    }
+
+    return res.status(500).json({ error: erroFinal });
+}
